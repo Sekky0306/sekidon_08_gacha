deb566d17f3ea4e5edd36450c61ac039348f0022
YukiSekido <step1336@gmail.com>
2021-05-13 13:52:43 +0900

    Initial Commit

---
+++ GachaTech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GachaTech
+//
+//  Created by 関戸優紀 on 2021/05/13.
+//  Copyright © 2021 app.sekido.sekidon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
